43077a1054f7961e8a4ca1af6b5fbf1cb826d8ce
yousayeh <younes.sayeh@eduvaud.ch>
2023-02-08 15:15:07 +0100

    done the style and structure of the home page, insert model and superusers

--- P_APPRO/DepensesPerso/views.py
+++ P_APPRO/DepensesPerso/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from DepensesPerso.models import User
 
 # Create your views here.
 def index(request):
-    context = {"message": "Hello World !"}
-    template = loader.get_template("index.html")
-    return HttpResponse(template.render(context, request))
+    users = User.objects.all()
+    return render(request, 'index.html', context={"user": users})
 
 # def index(request):
 #     return render(request, 'index.html')
