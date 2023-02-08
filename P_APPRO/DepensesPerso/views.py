528fc2d51b1fa4f2798cb792cded0c3f266634c9
aless <61836900+alessdangelo@users.noreply.github.com>
2023-02-08 14:51:21 +0100

    Added comments on views.py
    Added comments in order to make it understandable by others

--- P_APPRO/DepensesPerso/views.py
+++ P_APPRO/DepensesPerso/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.template import loader #Django module letting us load HTML templates
 
 # Create your views here.
 def index(request):
-    context = {"message": "Hello World !"}
-    template = loader.get_template("index.html")
-    return HttpResponse(template.render(context, request))
+    context = {"message": "Hello World !"} #Variable that can be added to the template before loading it
+    template = loader.get_template("index.html") #Get template index.html
+    return HttpResponse(template.render(context, request)) #request the loading of the template and return it
 
 # def index(request):
 #     return render(request, 'index.html')
