66154975c80665d82e1687b91a5d9fb29881f9cb
yousayeh <younes.sayeh@eduvaud.ch>
2023-02-08 16:21:21 +0100

    List the users and allows to add users in the database

--- P_APPRO/DepensesPerso/views.py
+++ P_APPRO/DepensesPerso/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from DepensesPerso.models import User
 
-# Create your views here.
+# Send the index and get the users from database.
 def index(request):
     users = User.objects.all()
-    return render(request, 'index.html', context={"user": users})
+    username = request.POST.get("username")
+    return render(request, 'index.html', context={"users": users})
 
-# def index(request):
-#     return render(request, 'index.html')
+# Add a user with the form
+def addUser(request):
+    username = request.POST.get("username")
+    User.objects.create(useName=username)
+    return redirect('index')
