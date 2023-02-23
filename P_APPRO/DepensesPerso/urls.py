5deb21ab0eb28c6f26518b927f8acd31526d61bb
aless <61836900+alessdangelo@users.noreply.github.com>
2023-02-23 16:32:13 +0100

    Created empty page "addSpending"
    + Page "addSpending"
    + Enhanced a few comments,

--- P_APPRO/DepensesPerso/urls.py
+++ P_APPRO/DepensesPerso/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('addSpending/', views.addSpending, name='addSpending'),
     path('addUser/', views.addUser, name="addUser"),
     path('deleteUser/<int:userId>/', views.deleteUser, name="deleteUser")
 ]
--- P_APPRO/DepensesPerso/views.py
+++ P_APPRO/DepensesPerso/views.py
@@ -5,34 +5,47 @@ from DepensesPerso.models import User
 from django.contrib import messages
 import string
 
+# Show pages
+
 # Send the index and get all the users from database.
 def index(request):
     users = User.objects.all()
-    return render(request, 'index.html', context = {"users": users})
+    return render(request, 'index.html', context={"users": users})
+
+# Send the addSpending page and get all the users from database.
+
+
+def addSpending(request):
+    users = User.objects.all()
+    return render(request, 'addSpending.html')
+
+
+# Page Functions
 
 # Add a user with the form
 def addUser(request):
-    MAX_LENGTH_USERNAME = 12 # const for the max allows characters
+    MAX_LENGTH_USERNAME = 12  # const for the max number of allowed characters
     username = request.POST.get("username")
 
-    # Check for the username's length and not empty
+    # Check for the username's length and if not empty
     if len(username) <= MAX_LENGTH_USERNAME and username:
-        # Delete the space charactere at start of the user's input
-        username = username.strip() 
+        # Delete any space at start from the user's input
+        username = username.strip()
         if username:
-            # Add the username's input in the database fiels 'useName'
-            User.objects.create(useName = username) 
-        else :
+            # Add the username's input in the database field 'useName'
+            User.objects.create(useName=username)
+        else:
             messages.error(request, "Veuillez ne pas commencer par un espace")
     else:
-        messages.error(request, "Veuillez respecter le nombre de caractères autorisés (minimum 1 et maximum 13)")
+        messages.error(
+            request, "Veuillez respecter le nombre de caractères autorisés (minimum 1 et maximum 13)")
 
     return redirect('index')
 
 
 # Delete a user with the link
 def deleteUser(request, userId):
-    username = get_object_or_404(User, pk = userId)
+    username = get_object_or_404(User, pk=userId)
     username.delete()
 
     return redirect('index')
